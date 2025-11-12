a3284dacbf90e945386ba50fca0bcf3621499636
MusratJahan408 <musratjahaneva@gmail.com>
2025-11-12 10:56:20 +0600

    data update for put methode added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config()
-const app = express()
-const port =process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.two3kqb.mongodb.net/?appName=Cluster0`;
 
@@ -17,29 +16,48 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const db = client.db("model-db")
-    const modelCollection = db.collection("models")
+    const db = client.db("model-db");
+    const modelCollection = db.collection("models");
 
-    app.get("/models", async(req,res)=>{
-        const result =await modelCollection.find().toArray()
-        res.send(result)
-    })
+    app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.post("/models", async(req,res)=>{
-        const data =req.body
-        const result = await modelCollection.insertOne(data)
-        res.send(result)
-    })
+    app.get("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await modelCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    app.put("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const update = {
+        $set: data,
+      };
+      const result = await modelCollection.updateOne(filter, update);
+      res.send(result);
+    });
+
+    app.post("/models", async (req, res) => {
+      const data = req.body;
+      const result = await modelCollection.insertOne(data);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -47,12 +65,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
